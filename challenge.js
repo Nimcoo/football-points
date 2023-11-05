6a0cdc3ae2991f9ffd13e8feeec4a897f98e026b
NIMCOO <nimco77001@gmail.com>
2023-11-05 13:41:52 +0000

    Finished

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function fotballpoints(Num1,Num2,Num3){
+
+    console. log( num1+num3*num2)
+  
+  }
+   fotballpoints((7,3),(3,2),(3,0))
+  
